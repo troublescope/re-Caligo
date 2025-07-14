039cbddca3624627b557e5ed0bca3b472041dc34
Troublescope <tomiprs25@gmail.com>
2025-07-14 18:17:26 +0700

    fix(database): set custom DNS resolver for Termux environments
    - Detects if running under Termux via TERMUX__UID environment variable.
    - If in Termux, sets dnspython's default resolver to use 8.8.4.4 as nameserver.
    - This helps avoid DNS resolution issues on Termux, especially with MongoDB connections.
    
    Signed-off-by: Troublescope <tomiprs25@gmail.com>

--- caligo/core/database_provider.py
+++ caligo/core/database_provider.py
@@ -1,3 +1,4 @@
+import os
 from typing import TYPE_CHECKING, Any
 
 from .base import CaligoBase
@@ -6,11 +7,19 @@ from .database import AsyncClient, AsyncDatabase
 if TYPE_CHECKING:
     from .bot import Caligo
 
+IS_TERMUX = False
+if os.getenv("TERMUX__UID"):
+    IS_TERMUX = True
 
 class DatabaseProvider(CaligoBase):
     db: AsyncDatabase
 
     def __init__(self: "Caligo", **kwargs: Any) -> None:
+        if IS_TERMUX:
+            import dns.resolver
+            dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+            dns.resolver.default_resolver.nameservers = ["8.8.4.4"]
+            
         client = AsyncClient(self.config["bot"]["db_uri"], connect=False)
         self.db = client.get_database("CALIGO")
 
