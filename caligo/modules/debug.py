73920741221b409521795eb59883bf9b1eca0363
Troublescope <tomiprs25@gmail.com>
2025-07-16 22:24:20 +0700

    fix(caligo/modules/debug.py): prevent debug output from being printed to console
    Signed-off-by: Troublescope <tomiprs25@gmail.com>

--- caligo/modules/debug.py
+++ caligo/modules/debug.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import traceback
+from contextlib import redirect_stdout
 from html import escape
 from typing import Any, ClassVar, Optional, Tuple
 
@@ -158,7 +159,8 @@ class Debug(module.Module):
             }
 
             try:
-                return "", await meval(code, globals(), **eval_vars)
+                with redirect_stdout(out_buf):
+                    return "", await meval(code, globals(), **eval_vars)
             except Exception as e:  # skipcq: PYL-W0703
                 # Find first traceback frame involving the snippet
                 first_snip_idx = -1
